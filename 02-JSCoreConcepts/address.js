7147e0b01fef6e1326e709e38e5b874cafa6757c
Jan Ignasik <jani@qualtrics.com>
2020-03-20 21:13:15 +0100

    addess exercise

--- 02-JSCoreConcepts/address.js
+++ 02-JSCoreConcepts/address.js
@@ -6,7 +6,16 @@ city = 'Kraków';
 zip = '30-781';
 country = 'Poland';
 
+const stringWithoutFormatting = title + ' ' +  name +  ' ' + surname + '\n'
+    + 'ul.' +  ' ' + street + '\n' + zip +  ' ' + city + '\n' + country
+const stringWithFormatting = `${title} ${name} ${surname}
+ul. ${street}
+${zip} ${city}
+${country}`
 
+
+console.log(stringWithoutFormatting)
+console.log(stringWithFormatting)
 /* 
 dr Mateusz Kubicki
 ul. Długa 15
