927405d0396c8fdbb81e7a18c9f80ddf357fd662
Jan Ignasik <jani@qualtrics.com>
2020-03-20 21:21:37 +0100

    heron exercise

--- 02-JSCoreConcepts/heron.js
+++ 02-JSCoreConcepts/heron.js
@@ -6,6 +6,11 @@ h = 7
 
 // Pole trókąta o bokach ..., ... oraz ... wynosi ... .
 
-let a = () => "aaaa";
+let heron = (a, b, c) => {
+    const s = (a + b + c)/2
 
-console.log( a() );
+    const area = Math.sqrt(s*(s - a)*(s - b)*(s - c))
+
+    return `Pole trókąta o bokach ${a}, ${b} oraz ${c} wynosi ${area}.`
+}
+console.log( heron(b, c, h) );
